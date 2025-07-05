c521b42d093a4f9a19f8fbd0ead062d1bd508595
Esraa Ahmed <92982867+EsraaAhmd@users.noreply.github.com>
2025-07-05 16:41:27 +0300

    Updated visualize.py

--- visualize.py
+++ visualize.py
@@ -1,23 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# Step 1: Load CSV
+# Load subnet analysis data
 df = pd.read_csv("subnet_report.csv")
 
-df["CIDR"] = pd.Categorical(df["CIDR"], categories=df["CIDR"].unique(), ordered=True)
-grouped = grouped = df.groupby("CIDR", sort=False, observed=False)["Usable Hosts"].sum().reset_index()
-
-
-
+# Plot bar chart
 plt.figure(figsize=(10, 5))
-bars = plt.bar(grouped["CIDR"].astype(str), grouped["Usable Hosts"], color="pink")
-plt.xlabel("Subnet (CIDR)", fontsize=10, fontweight='bold', color="#333")
-plt.ylabel("Total Usable Hosts", fontsize=10, fontweight='bold', color="#333")
-plt.title("Usable Hosts per Subnet", fontsize=14, fontweight='bold', color="#333")
+bars = plt.bar(df["CIDR"].astype(str), df["Usable Hosts"], color="pink")
+
+# Labels and title
+plt.xlabel("Subnet (CIDR)", fontsize=10, fontweight='bold')
+plt.ylabel("Total Usable Hosts", fontsize=10, fontweight='bold')
+plt.title("Usable Hosts per Subnet", fontsize=14, fontweight='bold')
 plt.xticks(rotation=90)
 
-
+# Save and show
 plt.tight_layout()
 plt.savefig("network_plot.png")
 plt.show()
